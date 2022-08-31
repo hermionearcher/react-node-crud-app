2a3c709aa5a7a7b6e6751a5e52b2a1d68caadb75
hermionearcher <hermione-archer@hotmail.co.uk>
2022-08-31 14:51:31 +0100

    project commit

---
+++ mern-stack-crud/src/Components/create-student.component.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import StudentForm from './StudentForm'
+
+const CreateStudent = () => {
+        const [formValues, setFormValues] = 
+          useState({ name: '', email: '', rollno: '' })
+        // onSubmit handler
+        const onSubmit = studentObject => {
+          axios.post(
+      'http://localhost:3000/students/create-student', 
+          studentObject)
+            .then(res => {
+              if (res.status === 200)
+                alert('Student successfully created')
+              else
+                Promise.reject()
+            })
+            .catch(err => alert('Something went wrong'))
+        }
+    return (
+        <StudentForm initialValues={formValues}
+            onSubmit={onSubmit}
+            enableReinitialize>
+            Create Student
+        </StudentForm>
+    )
+}
+
+export default CreateStudent
